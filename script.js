75b977ec261baabc36f035f18783cd235531e654
Tauseef Afraz <tauseef.afraz.1@gmail.com>
2023-12-17 02:11:29 -0600

    imma cry cause idk why the contact form isnt working

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 //Loads the header and footer into the proper positions. This definitely works, regardless of what the compiler says.
 $(document).ready(function () {
-    var rellax = new Rellax('.rellax');
-    $("#navbar").load("header.html");
-    $("#footer").load("footer.html");
+  var rellax = new Rellax('.rellax');
+  $("#navbar").load("header.html");
+  $("#footer").load("footer.html");
 });
 
 /**
@@ -15,45 +15,45 @@ const errorMessage = document.querySelector('.error-message');
 const sentMessage = document.querySelector('.sent-message');
 
 contactForm.addEventListener('submit', function (event) {
-    event.preventDefault(); // Prevent the default form submission
+  event.preventDefault();
 
-    const recaptchaResponse = document.getElementById('g-recaptcha-response').value;
+  const recaptchaResponse = document.getElementById('g-recaptcha-response').value;
 
-    if (!recaptchaResponse) {
-        errorMessage.style.display = 'block';
-        loadingMessage.style.display = 'none'; // Hide loading message
-        sentMessage.style.display = 'none'; // Hide success message
-        return;
-    }
+  if (!recaptchaResponse) {
+    errorMessage.style.display = 'block';
+    loadingMessage.style.display = 'none'; // Hide loading message
+    sentMessage.style.display = 'none'; // Hide success message
+    return;
+  }
 
-    loadingMessage.style.display = 'block'; // Show loading message
+  loadingMessage.style.display = 'block'; // Show loading message
 
-    const formData = new FormData(this);
-    const data = Object.fromEntries(formData);
-    data.gCaptchaResponse = recaptchaResponse;
+  const formData = new FormData(this);
+  const data = Object.fromEntries(formData);
+  data.gCaptchaResponse = recaptchaResponse;
 
-    fetch(url, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json', // Update content type
-        },
-        body: JSON.stringify(data),
-    })
-    .then((res) => res.json())
-    .then((data) => {
-        console.log('Successful', data);
-        contactForm.reset();
-        loadingMessage.style.display = 'none'; // Hide loading message
-        sentMessage.style.display = 'block'; // Show success message
-        errorMessage.style.display = 'none'; // Hide error message
-        setTimeout(function () {
-            sentMessage.style.display = 'none'; // Hide success message after a few seconds
-        }, 5000);
-    })
-    .catch((err) => {
-        console.log('err', err);
-        loadingMessage.style.display = 'none'; // Hide loading message
-        errorMessage.style.display = 'block'; // Show error message
-        sentMessage.style.display = 'none'; // Hide success message
-    });
+  fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'text/plain;charset=utf-8',
+    },
+    body: JSON.stringify(data),
+  })
+  .then((res) => res.json())
+  .then((data) => {
+    console.log('Successful', data);
+    contactForm.reset();
+    loadingMessage.style.display = 'none'; // Hide loading message
+    sentMessage.style.display = 'block'; // Show success message
+    errorMessage.style.display = 'none'; // Hide error message
+    setTimeout(function () {
+      sentMessage.style.display = 'none'; // Hide success message after a few seconds
+    }, 5000);
+  })
+  .catch((err) => {
+    console.log('err', err);
+    loadingMessage.style.display = 'none'; // Hide loading message
+    errorMessage.style.display = 'block'; // Show error message
+    sentMessage.style.display = 'none'; // Hide success message
+  });
 });
