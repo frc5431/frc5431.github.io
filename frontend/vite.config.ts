f3ca650e86af7482ea23fd4cfb5bb0caf4fceef7
micw1417 <wu355049@gmail.com>
2025-07-17 15:09:56 -0500

    package updates and image + assets compression

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,12 +1,44 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { compression } from 'vite-plugin-compression2'
+import viteImagemin from 'vite-plugin-imagemin'
 
 const isDocker = process.env.DOCKER === 'true'
 
 // https://thedkpatel.medium.com/dockerizing-react-application-built-with-vite-a-simple-guide-4c41eb09defaexport default defineConfig({
 export default defineConfig({
  base: "/",
- plugins: [react()],
+ plugins: [react(),  compression({
+      algorithms: ['brotli', 'gzip'],
+   }),
+   viteImagemin({
+        gifsicle: {
+          optimizationLevel: 1,
+          interlaced: false,
+        },
+        optipng: {
+          optimizationLevel: 3,
+        },
+        mozjpeg: {
+          quality: 20,
+        },
+        pngquant: {
+          quality: [0.9, 0.9],
+          speed: 8,
+        },
+        svgo: {
+          plugins: [
+            {
+              name: 'removeViewBox',
+            },
+            {
+              name: 'removeEmptyAttrs',
+              active: false,
+            },
+         ],
+      },
+   }),
+ ],
  preview: {
   port: 8080,
   strictPort: true,
