205b823dd3d17934e9957864cdc53221d5a3fa7b
micw1417 <wu355049@gmail.com>
2025-08-02 13:14:45 -0500

    got rid of visualizer and build rollup thing from vite that broke stuff

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig, type PluginOption } from "vite";
 import react from "@vitejs/plugin-react";
 import { compression } from "vite-plugin-compression2";
-import { visualizer } from "rollup-plugin-visualizer";
+// import { visualizer } from "rollup-plugin-visualizer";
 
 const isDocker = process.env.DOCKER === "true";
 const HOST = process.env.HOST;
@@ -13,24 +13,9 @@ export default defineConfig({
     compression({
       algorithms: ["brotli", "gzip"],
     }) as PluginOption,
-    visualizer({ open: true }) as PluginOption,
   ],
   server: {
     host: isDocker ? true : "localhost",
     allowedHosts: HOST ? [HOST] : [],
-  },
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes("node_modules")) {
-            if (id.includes("react-icons")) return "react-icons";
-            if (id.includes("react-router-dom")) return "router";
-            if (id.includes("react-dom")) return "react-dom";
-            return "vendor";
-          }
-        },
-      },
-    },
   },
 });
