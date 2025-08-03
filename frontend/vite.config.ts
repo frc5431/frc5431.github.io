338148ceff41f46ef919b90d29ea67eed7688b34
micw1417 <80129528+micw1417@users.noreply.github.com>
2025-08-03 13:01:47 -0500

    Performance caddy (#14)
    * started working on readme, will do more. Other changes include
    made all images now use avif.
    added visualizer to see what takes up what but unsure if its actually helper
    will work on adding caddy instead of using serve as the webserver
    
    * got rid of visualizer and build rollup thing from vite that broke stuff
    
    * commit to add caddy I'm like 80% sure this will work
    
    * Not everything got staged oops

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,7 @@
-import { defineConfig } from "vite";
+import { defineConfig, type PluginOption } from "vite";
 import react from "@vitejs/plugin-react";
 import { compression } from "vite-plugin-compression2";
+// import { visualizer } from "rollup-plugin-visualizer";
 
 const isDocker = process.env.DOCKER === "true";
 const HOST = process.env.HOST;
@@ -8,13 +9,13 @@ const HOST = process.env.HOST;
 export default defineConfig({
   base: "/",
   plugins: [
-    react(),
+    react() as PluginOption,
     compression({
       algorithms: ["brotli", "gzip"],
-    }),
+    }) as PluginOption,
   ],
   server: {
     host: isDocker ? true : "localhost",
-    allowedHosts: [HOST || "frc5431.wucode.org"],
+    allowedHosts: HOST ? [HOST] : [],
   },
 });
